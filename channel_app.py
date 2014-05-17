da04ce3f2a94db93cb8e1aad3aecc8f1a2e7f4fe
brian <brian.netmad@gmail.com>
2014-05-17 17:51:27 +0800

    add file.

---
+++ channel_app.py
@@ -0,0 +1,15 @@
+﻿# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')  
+def hello_world():  
+    return "Hello World"
+
+@app.route('/channel/<id>/')
+def channel_get(id=None):
+    if id:
+        return render_template("index.html", data=id)
+
+if __name__ == '__main__':  
+    app.run(host='61.160.251.17', port=3690)  
